b06eaa747692320be24ec403b01fb39eab99fd79
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2023-12-11 10:47:17 +0000

    first commit

---
+++ handlers/handlers.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world!\n")
+}
+
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Article...\n")
+}
+
+func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	queryMap := req.URL.Query()
+
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+
+	resString := fmt.Sprintf("Article List (page: %d)\n", page);
+	io.WriteString(w, resString)
+}
+
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Article Detail...\n")
+}
+
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Nice...\n")
+}
+
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Comment...\n")
+}
