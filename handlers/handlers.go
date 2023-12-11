77cf7ad38d0851f9b7e329fda15a0d13ff92d317
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2023-12-11 12:07:26 +0000

    リクエストをjsonへデコード

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -15,7 +16,28 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	article := models.Article1
+	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
+	if err != nil {
+		http.Error(w, "cannot get content length", http.StatusInternalServerError);
+		return
+	}
+	
+	reqBodyBuffer := make([]byte, length)
+
+	if _, err := req.Body.Read(reqBodyBuffer); !errors.Is(err, io.EOF) {
+		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
+		return
+	}
+
+	defer req.Body.Close()
+
+	var reqArticle models.Article
+	if err := json.Unmarshal(reqBodyBuffer, &reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+		return
+	}
+
+	article := reqArticle
 	jsonData, err := json.Marshal(article)
 
 	if err != nil {
