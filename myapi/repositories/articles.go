10eceff6d2db214577a754e5f32b77b3cd1af708
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2023-12-16 10:52:47 +0900

    記事一覧取得処理実装

--- myapi/repositories/articles.go
+++ myapi/repositories/articles.go
@@ -7,6 +7,10 @@ import (
 	"github.com/tanakalucky/golang-test/models"
 )
 
+const (
+	articleNumPerPage = 5
+)
+
 func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
 	const sqlStr = `
 		insert into articles
@@ -30,4 +34,39 @@ func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
 	newArticle.UserName = article.UserName
 
 	return newArticle, nil
+}
+
+func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
+	const sqlStr = `
+		select
+			article_id,
+			title,
+			contents,
+			username,
+			nice
+		from
+			articles
+		limit ?
+		offset ?
+	`
+
+	offset := articleNumPerPage * (page - 1)
+
+	rows, err := db.Query(sqlStr, articleNumPerPage, offset)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	articles := make([]models.Article, 0)
+	for rows.Next() {
+		var article models.Article
+
+		rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName)
+		articles = append(articles, article)
+	}
+
+	return articles, nil
 }
