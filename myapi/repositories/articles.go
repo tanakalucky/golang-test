2278bc9d6f9ceed0d306ad997034dc3e0e7b347e
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2023-12-16 11:03:00 +0900

    記事詳細取得処理実装

--- myapi/repositories/articles.go
+++ myapi/repositories/articles.go
@@ -70,3 +70,37 @@ func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
 
 	return articles, nil
 }
+
+func SelectArticleDetail(db *sql.DB, articleID int) (models.Article, error) {
+	const sqlStr = `
+		select
+			article_id,
+			title,
+			contents,
+			username,
+			nice
+		from
+			articles
+		where
+			article_id = ?;
+	`
+
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		return models.Article{}, err
+	}
+
+	var article models.Article
+	var createdTime sql.NullTime
+	err := row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
+	}
+
+	return article, nil
+}
